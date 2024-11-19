191734bfa1bfe668337e4717673691aba49fd773
Emilse Arias <emilsearias@gmail.com>
2024-11-19 10:41:37 -0300

    creamos una instancia de la clase contacto

--- js/app.js
+++ js/app.js
@@ -3,3 +3,4 @@ import Contacto from "./classContacto.js"
 const nuevoContacto = new Contacto('juan','perez', 'juanperez@mail.com','1234567890','juancho','Tucuman, argentina', 'https://universe.rollingcodeschool.com')
 
 console.log(nuevoContacto)
+
--- js/classContacto.js
+++ js/classContacto.js
@@ -20,3 +20,4 @@ export default class Contacto{
 
     //agregar los get y set
 }
+
