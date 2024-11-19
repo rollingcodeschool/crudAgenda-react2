6a4904534cc2f492b1d7c1392e67a57fd12cf5eb
Emilse Arias <emilsearias@gmail.com>
2024-11-19 11:34:13 -0300

    agregamos el contacto en nuestra lista de contactos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 import Contacto from "./classContacto.js"
 
 //declaro variables globales
-const nuevoContacto = new Contacto('juan','perez', 'juanperez@mail.com','1234567890','juancho','Tucuman, argentina', 'https://universe.rollingcodeschool.com')
+
 
 const modalContacto = new bootstrap.Modal(document.getElementById('contactModal'));
 const btnAgregar = document.querySelector('.btn-primary')
+const formulario = document.getElementById('contactForm');
+const nombre = document.querySelector('#nombre')
+const apellido = document.querySelector('#apellido')
+const email = document.querySelector('#email')
+const apodo = document.querySelector('#apodo')
+const telefono = document.querySelector('#telefono')
+const direccion = document.querySelector('#direccion')
+const imagen = document.querySelector('#urlImagen')
+const listaContactos =[]
 
 //funciones
 function abrirModalContacto(){
     modalContacto.show()
 }
 
+function crearContacto(e){
+    e.preventDefault();
+    //validar los datos del formulario
+    //crear el objeto
+    const nuevoContacto = new Contacto(nombre.value,apellido.value, email.value, telefono.value, apodo.value, direccion.value, imagen.value)
+    //guardar el contacto nuevo en la lista de contactos
+    listaContactos.push(nuevoContacto)
+    console.log(listaContactos)
+}
+
 //el resto de la logica del proyecto
-btnAgregar.addEventListener('click', abrirModalContacto)
+btnAgregar.addEventListener('click', abrirModalContacto);
+formulario.addEventListener('submit', crearContacto)
