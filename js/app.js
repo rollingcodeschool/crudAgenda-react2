4ee182fe45da3e45c413df3c1fe11950a84ac6b5
Emilse Arias <emilsearias@gmail.com>
2024-11-19 11:11:40 -0300

    abrimos la ventana modal cuando el usuario quiere crear un contacto

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 import Contacto from "./classContacto.js"
 
+//declaro variables globales
 const nuevoContacto = new Contacto('juan','perez', 'juanperez@mail.com','1234567890','juancho','Tucuman, argentina', 'https://universe.rollingcodeschool.com')
 
-console.log(nuevoContacto)
+const modalContacto = new bootstrap.Modal(document.getElementById('contactModal'));
+const btnAgregar = document.querySelector('.btn-primary')
 
+//funciones
+function abrirModalContacto(){
+    modalContacto.show()
+}
+
+//el resto de la logica del proyecto
+btnAgregar.addEventListener('click', abrirModalContacto)
