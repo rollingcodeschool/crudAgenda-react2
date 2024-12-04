5d13a99cf8c79e10fbed4eca31a7b05479f858f4
Emilse Arias <emilsearias@gmail.com>
2024-12-04 17:38:06 -0300

    agregue el los datos del detalle de contacto

--- js/mostrarDetalleContacto.js
+++ js/mostrarDetalleContacto.js
@@ -12,4 +12,23 @@ const contactoBuscado = agenda.find((contacto)=> contacto.id ===  id)
 console.log(contactoBuscado)
 
 //TODO: dibujar en la card horizontal los datos del contacto encontrado
-
+const cardContacto = document.querySelector('#contact');
+console.log(cardContacto)
+cardContacto.innerHTML = `<div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${contactoBuscado.imagen}" class="img-fluid rounded-start" alt="${contactoBuscado.apellido}, ${contactoBuscado.nombre} ">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Contacto: ${contactoBuscado.nombre}, ${contactoBuscado.apellido}</h5>
+              <ul>
+                <li><b>Apodo:</b> ${contactoBuscado.apodo} </li>
+                <li><b>Email:</b> ${contactoBuscado.email} </li>
+                <li><b>Teléfono:</b> ${contactoBuscado.telefono} </li>
+                <li><b>Dirección:</b> ${contactoBuscado.direccion} </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>`
